53c98608f65cc8976d057b2b8d48989ee0b5c70b
Osburn,Eric <osburn.eric@gmail.com>
2025-01-12 18:56:00 -0700

    Falling Spirals early version.. Not quite happy

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,19 @@ use std::time;
 
 use nannou::prelude::*;
 use nannou::noise::*;
+use nannou::draw::*;
+use nannou::rand::*;
+use nannou::lyon::{geom::euclid::Point2D, path::Path};
 use nannou_egui::{self, egui, Egui};
 
+// Window size in Points, coordinates from (-WIN_d / 2, WIN_d / 2)
 const WIN_WIDTH: u32 = 1000;
 const WIN_HEIGHT: u32 = 1000;
 
 fn main() {
     nannou::app(model)
         .update(update)
+        .loop_mode(LoopMode::wait())
         .run();
 }
 
@@ -115,56 +120,100 @@ fn update(_app: &App, model: &mut Model, update: Update) {
 fn view(app: &App, model: &Model, frame: Frame) {
     let settings = &model.settings;
 
-    let draw = app.draw();
+    let draw: Draw = app.draw();
 
     draw.background().color(nannou::color::BLACK);
 
-    let gridsize_x = WIN_WIDTH as f64 / settings.nums as f64;
-    let gridsize_y = WIN_HEIGHT as f64 / settings.nums as f64;
+    // How many lines to draw
+    const NUM_LINES: usize = 32;
+    // How many drawn points in a line
+    const NUM_PTS: usize = 256;
+    // "Start" radius
+    const START_RAD: f32 = 400.0;
+    // Number of segments to split the start to
+    const NUM_SEG: usize = 4;
+    // Corresponding arclen
+    const SEG_AL: f32 = 2.0 * PI / NUM_SEG as f32;
+    // Proportion of segment to start points in
+    const SEG_START_PRO: f32 = 0.75;
+    // Corresponding arclen
+    const SEG_START_AL: f32 = SEG_AL * SEG_START_PRO;
+    // Global offset for segment start
+    let SEG_OFF: f32 = random_range(0.0, 2.0 * PI);
+    // "Stop" radius as percentage of START_RAD
+    const STOP_RAD_PERC: f32 = 0.125;
+    // "Wind-in" speed -> Line will 0..WIND_AL
+    const WIND_AL: f32 = 4.0 * PI;
 
-    for row in 0..(settings.nums) {
-        let pos_y = gridsize_y * ( 0.5 + row as f64 ) - (WIN_HEIGHT as f64 / 2.0);
+    // Draw some number of lines
+    let mut lines: Vec<Path> = Vec::with_capacity(NUM_LINES);
 
-        let pos_y_norm = pos_y * 2.0 / WIN_HEIGHT as f64;
+    for ldx in 0..NUM_LINES {
+        let mut b = Path::builder();
 
-        for col in 0..(settings.nums) {
-            let pos_x = gridsize_x * ( 0.5 + col as f64 ) - (WIN_WIDTH as f64 / 2.0);
+        // Get "segment" to start in
+        let ss: usize = random_range(0, NUM_SEG);
+        // Some dist into that segment
+        let t0: f32 = random_range(0.0, SEG_START_AL) + ss as f32 * SEG_AL + SEG_OFF;
 
-            let pos_x_norm = pos_x * 2.0 / WIN_WIDTH as f64;
+        // "Step" start point some number of times
+        // r = sin(x) + sigmoid(x)
+        // t = x
 
-            let nxc = pos_x_norm * settings.nxf;
-            let nyc = pos_y_norm * settings.nyf;
+        let wav_off: f32 = random_range(0.0, 2.0 * PI);
+        let wav_mag: f32 = random_range(0.05, 0.1);
+        let wav_spd: f32 = random_range(48.0, 80.0);
+        let line_start: f32 = random_range(0.0, 0.5);
+        let line_end: f32 = random_range(0.5, 1.0);
 
-            let nx = model.noisex.get([nxc, nyc, model.noiseoff]) * settings.nxs;
-            let ny = model.noisey.get([nxc, nyc, model.noiseoff]) * settings.nys;
+        for pdx in 0..NUM_PTS {
+            // We will need pdx as a percentage
+            let pdx_perc = pdx as f32 / ( NUM_PTS - 1 ) as f32;
+            // Clip percentage 0..1 -> line_start..line_end
+            let pdx_perc = pdx_perc * (line_end - line_start) + line_start;
 
-            let ns = model.noises.get([nxc, nyc, model.noiseoff]);
-            let ns = (ns + 1.0) / 2.0;
-            let ns = gridsize_x.min(gridsize_y) * settings.nss * ns;
+            // Theta is a function of dist along the line
+            let t = t0 + WIND_AL * pdx_perc;
 
-            let nc = model.noisec.get([nxc, nyc, model.noiseoff]);
-            let nc = (nc + 1.0) / 2.0;
+            // Sigmoid coord is rescaled 0..NUM_PTS -> -4..10
+            let sig_coord = pdx_perc * 14.0 - 4.0;
+            // Sigmoid is a logistic func of the sig_coord
+            let sig = 1.0 / ( 1.0 + (-1.0 * sig_coord).exp() );
+            // Flip around y=0.5
+            let sig = (sig - 0.5) * -1.0 + 0.5;
+
+            // Wave the line in and out, but real smol
+            // TODO: Wave perpendicular to velocity
+            let wav: f32 = (pdx_perc * wav_spd + wav_off).sin() * wav_mag;
+            // Also scale the wave by the sigmoid, so it shrinks as we get closer to event horizon
+            let wav = wav * sig;
+
+            // Shift and squash sigmoid 0..1 -> STOP_RAD_PERC..1
+            let sig = sig * ( 1.0 - STOP_RAD_PERC ) + STOP_RAD_PERC;
 
 
-            draw.ellipse()
-                .xy(Vec2::new(( pos_x + nx ) as f32, ( pos_y + ny ) as f32))
-                .radius(ns as f32)
-                .color(nannou::color::gray(nc as f32));
+            let r = (sig + wav) * START_RAD;
+
+            if pdx == 0 {
+                b.begin(Point2D::new(r * t.cos(), r * t.sin()));
+            } else {
+                b.line_to(Point2D::new(r * t.cos(), r * t.sin()));
+            }
         }
+
+        b.end(false);
+
+        let p = b.build();
+        lines.push(p);
     }
 
-    /*
-    let mut b = Path::builder();
-    b.begin(Point2D::new(-225.0, 0.0));
-    b.quadratic_bezier_to(Point2D::new(-75.0, 100.0), Point2D::new(0.0, 0.0));
-    b.quadratic_bezier_to(Point2D::new(75.0, -100.0), Point2D::new(225.0, 0.0));
-    b.end(false);
-    let p = b.build();
-    draw.path()
-        .stroke()
-        .color(nannou::color::WHITE)
-        .events(p.iter());
-    */
+    for line in lines {
+        draw.path()
+            .stroke()
+            .color(nannou::color::WHITE)
+            .events(line.iter())
+            .finish();
+    }
 
     draw.to_frame(app, &frame)
         .unwrap();
